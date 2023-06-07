68fcf44e1f6ad1a1c0a033219ab116ff8b3aea7a
HelloXiuXiu <xiuxiuweb@gmail.com>
2023-06-07 17:42:53 +0300

    removed node_modules form remote rep

--- node_modules/@socketsupply/socket/dns.js
+++
@@ -1,9 +0,0 @@
-/**
- * @module dns
- * @notice This is a rexports of `dns/index.js` so consumers will
- * need to only `import dns from 'socket:dns'`
- */
-import * as exports from './dns.js'
-
-export * from './dns/index.js'
-export default exports
